1389a86790498178610ef59d8b6d9fcfe0fb0444
chripas <chri-pas@hotmail.com>
2020-03-10 10:33:14 +0000

    9_forkify

---
+++ 9_forkify_project/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/js/index.js'
+  ],
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: 'js/bundle.js'
+  // },
+	output: {
+    path: __dirname,
+    filename: 'dist/js/bundle.js'
+  },
+	// output: {
+	// path: __dirname,
+	// publicPath: '/',
+	// filename: 'bundle.js'
+	// },
+	// devServer: {
+	// 	contentBase: './dist'
+	// },
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: './dist/index.html',
+			template: './src/index.html'
+		})
+	],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+			}
+		]
+	}
+};
